df2426b7591cee0a9e09d7251971471d9950ec23
A1exsandro <alex.con100trado@gmail.com>
2023-06-26 15:37:54 -0400

    feat: basi config for api

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import api from './api/axiosConfig'
+
+
+function App() {
+  const [movies, setMovies] = useState()
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies")
+      setMovies(response.data)
+      console.log(response.data)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getMovies()
+  },[])
+
+  return (
+    <div className="App">
+     Hello!!!
+    </div>
+  );
+}
+
+export default App
